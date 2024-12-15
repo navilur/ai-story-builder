bd073c9bc06085153d353600cc85f0e4c28419fe
Navilur Rahman <navilur.rahman@gmail.com>
2024-12-16 01:16:45 +0600

    .

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@nextui-org/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Button color="primary">Hello</Button>
+      {/* https://youtu.be/bxhDP-e4NDA?t=1429 */}
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import { nextui } from "@nextui-org/react";
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: "#5253A3",
+            },
+          },
+        },
+        dark: {
+          colors: {},
+        },
+      },
+    }),
+  ],
+} satisfies Config;
